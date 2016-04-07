2c7ef738215933b0ec3518aed10a506f72c58ebf
serhiiwinl <sergiiwinl@gmail.com>
2016-04-07 13:05:36 +0300

    ios

--- ios/src/main/java/com/partypoker/poker/trackers/AppFlyerTrackerIOS.java
+++ ios/src/main/java/com/partypoker/poker/trackers/AppFlyerTrackerIOS.java
@@ -1,10 +1,14 @@
 package com.partypoker.poker.trackers;
 
 import com.partypoker.poker.trackers.concrete.AppFlyerTracker;
+import com.partypoker.poker.tracking.IBaseApplicationEvents;
+import com.partypoker.poker.tracking.ILoginEvents;
+import com.partypoker.poker.tracking.IUserActions;
 
 /**
  * Created by sliubetskyi on 4/6/16.
  */
+@TrackingList(value = {IBaseApplicationEvents.class, ILoginEvents.class, IUserActions.class})
 public class AppFlyerTrackerIOS extends AppFlyerTracker {
     @Override
     public void onAttachToApp(Object app) {
